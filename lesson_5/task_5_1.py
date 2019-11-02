ede62d0d28ef7ab7bded1b7dbdf66ad6db64ed2c
SergeyMakhotkin <ser9box@mail.ru>
2019-11-03 01:40:21 +0300

    сделал задание 1

--- lesson_5/task_5_1.py
+++ lesson_5/task_5_1.py
@@ -2,17 +2,46 @@
 # для каждого предприятия. Программа должна определить среднюю прибыль (за год для всех предприятий)
 # и отдельно вывести наименования предприятий, чья прибыль выше среднего и ниже среднего.
 
-from collections import namedtuple, defaultdict
+from collections import defaultdict, OrderedDict
 
-# Companies = namedtuple('Companies', 'name, margin', defaults=['unknown_name', 0])
+while True:
+    companies_number = input("Введите количество предприятий: \n")
+    try:
+        companies_number = int(companies_number)
+        if companies_number > 0:
+            break
+    except ValueError:
+        print("Неправильный ввод. Количество должно быть целым положительным числом")
+companies = defaultdict(list)  # создаем defaultdict в котором будет храниться информация о предприятиях
 
-number = int(input("Введите количество предприятий: \n"))
-companies = defaultdict(list)
+for company in range(companies_number):  # заполняем словарь
+    name = input(f"Введите название {company + 1}-компании: \n")  # цикл по предприятиям
+    for i in range(4):  # цикл по кварталам
+        while True:
+            margin = input(f"Введите прибыль компании '{name}' за {i + 1}-й квартал : \n")
+            try:
+                margin = float(margin)
+                break
+            except ValueError:
+                print("Недопустимое значение")
 
-for i in range(number):
-    name = input(f"Введите название {i+1}-компании: \n")
-    margin = float(input(f"Введите прибыль компании '{name}': \n"))
-    # companies[name] = margin
-    companies[name].append(margin)
-print(companies)
+        companies[name].append(margin)
+# print(companies)
 
+all_av_margin = 0
+for name, company_margin in companies.items():  # определяем среднюю годовую прибыль для каждого предприятия
+    avarege_margin = sum(company_margin) / 4
+    all_av_margin += avarege_margin
+    companies[name].append(avarege_margin)
+
+sorted_companies = OrderedDict(sorted(companies.items(), key=lambda x: x[1][4]))
+# сортированный список по средней годовой прибыли
+# print(sorted_companies)
+
+avarage = all_av_margin / companies_number
+print(f"Средняя прибыль компаний: {avarage:.2f}")
+for name, margin in sorted_companies.items():
+    if margin[-1] >= avarage:
+        print(f'Средняя прибыль компании "{name}": {margin[4]} (выше среднего)')
+    else:
+        print(f'Средняя прибыль компании "{name}": {margin[4]} (ниже среднего')
